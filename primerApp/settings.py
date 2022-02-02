eb03de3918883611527c735f84fe18b1dc27eeb4
AndyB <andybermudezcontact@gmail.com>
2022-02-01 20:40:59 -0600

    add file localsetting

--- primerApp/settings.py
+++ primerApp/settings.py
@@ -6,11 +6,14 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)tn13kq^+ut!@3m9_fp$pb-x87j$exab*n$a6mprcy!s_a8q(0'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,17 +78,6 @@ WSGI_APPLICATION = 'primerApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bdjango',
-        'USER': 'postgres',
-        'PASSWORD': 'HolaSpiderman.',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -127,3 +119,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from primerApp.local_settings import *
+except ImportError:
+    pass
